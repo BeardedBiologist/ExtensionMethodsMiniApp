19bc8f40556000584d29bcd75f47dcc906ebd1a1
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-05-03 12:13:27 +0200

    finished lesson. method overloads created

---
+++ ExtensionMethods/ConsoleHelper.cs
@@ -0,0 +1,50 @@
+﻿namespace ExtensionMethods
+{
+    public static class ConsoleHelper
+    {
+        public static string RequestString(this string message)
+        {
+            string output = "";
+
+            while (string.IsNullOrWhiteSpace(output))
+            {
+                Console.Write(message);
+                output = Console.ReadLine();
+            }
+
+            return output;
+        }
+
+
+
+        public static int RequestInt(this string message, int minVal, int maxVal)
+        {
+            return message.RequestInt(true, minVal, maxVal);
+        }
+
+        public static int RequestInt(this string message)
+        {
+            return message.RequestInt(false);
+        }
+
+        private static int RequestInt(this string message, bool useMinMax, int minVal = 0, int maxVal = 0)
+        {
+            int output = 0;
+            bool isValidInt = false;
+            bool isInValidRange = true;
+
+            while (isValidInt == false || isInValidRange == false)
+            {
+                Console.Write(message);
+                isValidInt = int.TryParse(Console.ReadLine(), out output);
+
+                if (useMinMax == true)
+                {
+                    isInValidRange = (output >= minVal && output <= maxVal);
+                }
+            }
+
+            return output;
+        }
+    }
+}
--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -6,6 +6,13 @@ namespace ExtensionMethods
     {
         static void Main(string[] args)
         {
+            PersonModel person = new PersonModel();
+
+            person.FirstName = "What is your first name?: ".RequestString();
+
+            person.LastName = "What is your last name?: ".RequestString();
+
+            person.Age = "What is your age: ".RequestInt(0, 120);
 
             Console.ReadLine();
         }
